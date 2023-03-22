908962178dc87c61ee8b8e52ad8db76f290f4afc
brunogomescarvalho <7bruno8@gmail.com>
2023-03-22 01:09:22 -0300

    firstCommit

---
+++ Calculadora/Program.cs
@@ -0,0 +1,193 @@
+﻿namespace Calculadora
+{
+    public class Program
+    {
+        static decimal PrimeiroValor;
+        static decimal SegundoValor;
+        static string[] Historico = new string[100];
+        static bool Continuar = true;
+        static string? Expressao;
+        static int Posicao = -1;
+        static int Opcao;
+
+        static void Main(string[] args)
+        {
+            while (Continuar)
+            {
+                Opcao = Menu();
+
+                if (VerificarSeEOperacao())
+                {
+                    PrimeiroValor = SolicitarEConverterValor("Informe o primeiro valor:");
+                    SegundoValor = SolicitarEConverterValor("Informe o segundo valor:");
+
+                    if (!VerificarNumeroValido())
+                    {
+                        MostrarMensagem("Para dividir, o segundo número não pode ser Zero!", true);
+                        continue;
+                    }
+                }
+
+                switch (Opcao)
+                {
+                    case 1:
+                        Somar();
+                        break;
+
+                    case 2:
+                        Subtrair();
+                        break;
+
+                    case 3:
+                        Dividir();
+                        break;
+
+                    case 4:
+                        Multiplicar();
+                        break;
+
+                    case 5:
+                        GerarTabuada();
+                        break;
+
+                    case 6:
+                        MostrarOperacoesEfetuadas();
+                        break;
+
+                    case 7:
+                        Sair();
+                        break;
+
+                    default:
+                        MostrarMensagem("Opção Inválida", true);
+                        break;
+                }
+            }
+        }
+
+        static decimal SolicitarEConverterValor(string mensagem)
+        {
+            Console.Clear();
+            Console.Write(mensagem);
+            return Convert.ToDecimal(Console.ReadLine());
+        }
+
+        static void MostrarMensagem(string mensagem, bool ehErro)
+        {
+            Console.Clear();
+
+            if (ehErro)
+                Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine(mensagem);
+
+            Console.ResetColor();
+
+            Console.ReadKey();
+
+        }
+
+        static void FinalizarOperacao()
+        {
+            IncluirNoHistorico();
+            MostrarMensagem(Expressao!, false);
+        }
+
+        static void Somar()
+        {
+            Expressao = $"{PrimeiroValor} + {SegundoValor} = {PrimeiroValor + SegundoValor}";
+            FinalizarOperacao();
+        }
+
+        static void Subtrair()
+        {
+            Expressao = $"{PrimeiroValor} - {SegundoValor} = {PrimeiroValor - SegundoValor}";
+            FinalizarOperacao();
+        }
+
+        static void Dividir()
+        {
+            Expressao = $"{PrimeiroValor} / {SegundoValor} = {Math.Round((PrimeiroValor / SegundoValor), 2)}";
+            FinalizarOperacao();
+        }
+
+        static void Multiplicar()
+        {
+            Expressao = $"{PrimeiroValor} x {SegundoValor} = {PrimeiroValor * SegundoValor}";
+            FinalizarOperacao();
+        }
+
+        static void GerarTabuada()
+        {
+            Console.Clear();
+            System.Console.WriteLine("Digite o número da tabuada que gostaria de gerar");
+            var opcao = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i <= 10; i++)
+            {
+                System.Console.WriteLine($"{opcao} x {i} = {opcao * i}");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void MostrarOperacoesEfetuadas()
+        {
+            Console.Clear();
+            System.Console.WriteLine(" -- Operações Salvas -- \n");
+
+            if (Posicao < 0)
+                MostrarMensagem("Nenhuma operação efetuada até o momento!", true);
+
+            else
+            {
+                foreach (string expressao in Historico)
+                {
+                    if (expressao != null)
+                        Console.WriteLine(expressao);
+                }
+                Console.ReadKey();
+            }
+        }
+
+        static bool VerificarSeEOperacao()
+        {
+            return (Opcao == 1 || Opcao == 2 || Opcao == 3 || Opcao == 4);
+        }
+
+        static bool VerificarNumeroValido()
+        {
+            return (Opcao == 3 && SegundoValor == 0) == false;
+        }
+
+
+        static void IncluirNoHistorico()
+        {
+            Posicao++;
+            Historico[Posicao] = Expressao!;
+        }
+
+        static int Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("--- [CALCULADORA] ---\nSelecione uma Opção\n");
+            Console.WriteLine("[1] Adição");
+            Console.WriteLine("[2] Subtração");
+            Console.WriteLine("[3] Divisão");
+            Console.WriteLine("[4] Multiplicação");
+            Console.WriteLine("[5] Gerar Tabuada");
+            Console.WriteLine("[6] Mostrar Operações Efetuadas");
+            Console.WriteLine("[7] Sair");
+
+            return int.Parse(Console.ReadLine()!);
+        }
+
+        static void Sair()
+        {
+            Continuar = false;
+        }
+
+
+    }
+
+}
---
+++ DiamanteDeX/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+namespace DiamanteDeX
+{
+    public class Program
+    {
+        static int EspacoMargem;
+        static int QuantidadeXLinhaCentral;
+        static int QuantidadeXPorLinha;
+        static int PosicaoPrimeiroX;
+        static bool Continuar = true;
+
+        public static void Main(string[] args)
+        {
+            while (Continuar)
+            {
+                QuantidadeXLinhaCentral = ReceberTamanhoTriangulo();
+
+                if (!EhNumeroImpar())
+                    continue;
+
+                CriarTrianguloSuperior();
+                CriarLinhaCentral();
+                CriarTrianguloInferior();
+                Console.ReadKey();
+
+            }
+
+        }
+
+        static int ReceberTamanhoTriangulo()
+        {
+            Console.Clear();
+            Console.WriteLine("--- Triângulo de X ---\n");
+            Console.WriteLine("Informe o tamanho do triângulo que gostaria de construir");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static bool EhNumeroImpar()
+        {
+            if (QuantidadeXLinhaCentral % 2 == 0)
+            {
+                Console.WriteLine("\nPara que o triângulo seja criado com sucesso o número precisa ser ímpar");
+                Console.ReadKey();
+                return false;
+            }
+            return true;
+        }
+
+        static void CriarTrianguloSuperior()
+        {
+            PosicaoPrimeiroX = (QuantidadeXLinhaCentral) / 2;
+            QuantidadeXPorLinha = 1;
+            EspacoMargem = PosicaoPrimeiroX - 1;
+
+            for (int i = 0; i <= PosicaoPrimeiroX - 1; i++)
+            {
+                for (int h = 0; h <= EspacoMargem; h++)
+                {
+                    Console.Write(" ");
+                }
+                for (int h = 0; h < QuantidadeXPorLinha; h++)
+                {
+                    Console.Write("X");
+                }
+
+                Console.WriteLine();
+                EspacoMargem--;
+                QuantidadeXPorLinha += 2;
+            }
+
+        }
+
+        static void CriarLinhaCentral()
+        {
+            for (int i = 0; i < QuantidadeXLinhaCentral; i++)
+            {
+                Console.Write("X");
+            }
+            Console.WriteLine();
+        }
+
+        static void CriarTrianguloInferior()
+        {
+            PosicaoPrimeiroX = (QuantidadeXLinhaCentral) / 2;
+            QuantidadeXPorLinha = QuantidadeXLinhaCentral - 2;
+            EspacoMargem = 1;
+
+            for (int i = 0; i < PosicaoPrimeiroX; i++)
+            {
+                for (int h = 0; h < EspacoMargem; h++)
+                {
+                    Console.Write(" ");
+                }
+                for (int h = 0; h < QuantidadeXPorLinha; h++)
+                {
+                    Console.Write("X");
+                }
+
+                Console.WriteLine();
+                EspacoMargem++;
+                QuantidadeXPorLinha -= 2;
+            }
+        }
+    }
+}
